4c4007cabb371ff1f53db64aa62635d83d2d7341
Olamide Kolawole <okolawole@ebsco.com>
2022-03-05 06:27:42 -0600

    first pass

---
+++ src/main/java/org/folio/javaagent/instrumentation/VertxClientSingletons.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.folio.javaagent.instrumentation;
+
+import io.opentelemetry.instrumentation.api.instrumenter.Instrumenter;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.core.http.HttpClientResponse;
+import org.folio.javaagent.common.VertxClientInstrumenterFactory;
+
+public final class VertxClientSingletons {
+
+  private static final Instrumenter<HttpClientRequest, HttpClientResponse> INSTRUMENTER =
+      VertxClientInstrumenterFactory.create(
+          "org.folio.javaagent.instrumentation.vertx-4.2",
+          new Vertx4HttpAttributesGetter(),
+          new Vertx4NetAttributesGetter());
+
+  public static Instrumenter<HttpClientRequest, HttpClientResponse> instrumenter() {
+    return INSTRUMENTER;
+  }
+
+  private VertxClientSingletons() {}
+}
